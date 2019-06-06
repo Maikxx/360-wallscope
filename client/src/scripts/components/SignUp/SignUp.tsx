f0dd23497a5f0652ffe28cad1899c32c8925cee5
jesperingels <jesper.ingels@gmail.com>
2019-06-06 14:39:05 +0200

    feature(Signup + Input): Create signup + refactor styling

--- client/src/scripts/components/SignUp/SignUp.tsx
+++ client/src/scripts/components/SignUp/SignUp.tsx
@@ -13,12 +13,48 @@ export class SignUp extends React.Component<Props> {
         const formAction = {
             action: '/',
         }
+        const inputs = {
+            email: {
+                type: 'email',
+                id: 'email-signup',
+                required: true,
+                placeholder: 'Email',
+                classname: 'Fieldset__input',
+                label: {
+                    text: 'Email',
+                    classname: 'Fieldset__label',
+                },
+            },
+            password: {
+                type: 'password',
+                id: 'password-signup',
+                required: true,
+                placeholder: 'Password',
+                classname: 'Fieldset__input',
+                label: {
+                    text: 'Password',
+                    classname: 'Fieldset__label',
+                },
+            },
+            password_repeat: {
+                type: 'password',
+                id: 'password-repeat-signup',
+                required: true,
+                placeholder: 'Repeat Password',
+                classname: 'Fieldset__input',
+                label: {
+                    text: 'Repeat Password',
+                    classname: 'Fieldset__label',
+                },
+            },
+        }
         return(
         <Form formAction={formAction}>
             <Fieldset classname='Fieldset'>
                 <Legend classname='Login__header' text='Login' />
                 <Input input={inputs.email} />
                 <Input input={inputs.password} />
+                <Input input={inputs.password_repeat} />
                 <Button styleOverride='red-button' type='submit'>Login</Button>
             </Fieldset>
         </Form>
