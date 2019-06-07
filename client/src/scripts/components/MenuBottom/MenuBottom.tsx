b0f191bfef20277099f9fa8fbddd443818a19694
Chelsea <chelsea.doeleman@gmail.com>
2019-06-07 13:29:29 +0200

    feature(MenuBottom): add MenuBottom

---
+++ client/src/scripts/components/MenuBottom/MenuBottom.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import classnames from 'classnames'
+import { Navigation } from '../Navigation/Navigation'
+import { routes } from '../../routes'
+import { NavLink } from '../Navlink/NavLink'
+import { Button } from '../Button/Button'
+
+interface Props {
+    className?: string
+    // iconName?: string
+    // color?: string
+    // route?: string
+    fullName?: string
+}
+
+export class MenuBottom extends React.Component<Props> {
+    public render() {
+        const { fullName } = this.props
+        return (
+            <Navigation className={this.getClassName()}>
+                <NavLink route={routes.App.index} iconName='boards' color='#CEC7EC'>Boards</NavLink>
+                <li>
+                    <Button type='button' iconName='search_big' color='#181631'/>
+                </li>
+                <NavLink route={routes.App.index} iconName='user' color='#CEC7EC'>{fullName}</NavLink>
+            </Navigation>
+        )
+    }
+
+    private getClassName() {
+        const { className } = this.props
+
+        return classnames('NavLink', { }, className)
+    }
+
+}
