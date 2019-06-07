9cb2509c847cbe2f64cc31919ee630b65e52dab2
Maikel <mjsleebos@gmail.com>
2019-06-07 13:32:59 +0200

    fix(links): Fix import link component

--- client/src/scripts/views/App/AppView.tsx
+++ client/src/scripts/views/App/AppView.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from '../../components/Link/Link'
 import { routes } from '../../routes'
 import { Home } from '../Home/Home'
 
