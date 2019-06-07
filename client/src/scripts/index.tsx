43d5c7b1251c2a345fc60e8bafa8d1319a68d037
Maikel <mjsleebos@gmail.com>
2019-06-07 13:41:51 +0200

    fix(routing): Attempt a fixing routing 🤔

--- client/src/scripts/index.tsx
+++ client/src/scripts/index.tsx
@@ -5,11 +5,11 @@ import { LoginView } from './views/Cover/LoginView'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { routes } from './routes'
 import { SignUpView } from './views/Cover/SignupView'
-import { AppView } from './views/App/AppView'
+import { Home } from './views/Home/Home'
 
 ReactDOM.render(
     <BrowserRouter>
-        <Route path={routes.App.index} component={AppView}/>
+        <Route path={routes.App.index} component={Home}/>
         <Route path={routes.Login.index} component={LoginView}/>
         <Route path={routes.Signup.index} component={SignUpView}/>
     </BrowserRouter>,
--- client/src/scripts/views/App/AppView.tsx
+++ client/src/scripts/views/App/AppView.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Link } from '../../components/Link/Link'
 import { routes } from '../../routes'
-import { Home } from '../Home/Home'
-
 interface Props {}
 
 export class AppView extends React.Component<Props> {
@@ -14,8 +12,6 @@ export class AppView extends React.Component<Props> {
                 <Link route={routes.App.index}>App</Link>
                 <Link route={routes.Login.index} iconName='user'>Login</Link>
                 <Link route={routes.Signup.index}>Signup</Link>
-
-                <Home />
 
             </React.Fragment>
         )
