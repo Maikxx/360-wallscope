8a0bb51968ba19f27ffa8925564d7a01e119903f
Chelsea <chelsea.doeleman@gmail.com>
2019-06-07 14:34:26 +0200

    feature(AppView): include MenuBottom

--- client/src/scripts/views/App/AppView.tsx
+++ client/src/scripts/views/App/AppView.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Search } from '../../components/Search/Search'
 import { routes } from '../../routes'
 import { Link } from '../../components/Link/Link'
+import { MenuBottom } from '../../components/MenuBottom/MenuBottom'
 
 interface Props {}
 
@@ -16,6 +17,8 @@ export class AppView extends React.Component<Props> {
                 <Link route={routes.Signup.index}>Signup</Link>
 
                 <Search />
+
+                <MenuBottom fullName='Chelsea' />
             </React.Fragment>
         )
     }
