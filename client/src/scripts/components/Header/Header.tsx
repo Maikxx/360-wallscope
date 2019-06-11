d1f0953e19a1a2ab7bd72c76696fd2de555a197a
jesperingels <jesper.ingels@gmail.com>
2019-06-11 11:47:52 +0200

    feature(Header): Add back button

--- client/src/scripts/components/Header/Header.tsx
+++ client/src/scripts/components/Header/Header.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import './Header.scss'
 import classnames from 'classnames'
+import { Icon } from '../Icon/Icon'
+import { Link } from 'react-router-dom'
 
 interface Props {
-    className?: string
+    className?: string,
+    back: boolean,
+    route: string
 }
 
 export class Header extends React.Component<Props> {
     public render () {
-        const { children } = this.props
+        const { children, back, route } = this.props
         return (
             <header className={this.getClassName()}>
+                <Link
+                to={route}
+                >
+                   {back && <Icon iconName='back' color='#FFFFFF' ></Icon>}
+                </Link>
                 {children}
             </header>
         )
