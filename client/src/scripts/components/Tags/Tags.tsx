70d2c3529ef0f38393e8204adc032f28d07f4a0c
Chelsea <chelsea.doeleman@gmail.com>
2019-06-12 10:19:27 +0200

    feature(Tags): add tags

---
+++ client/src/scripts/components/Tags/Tags.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import classnames from 'classnames'
+import './Tags.scss'
+import { Button, StyleType } from '../Button/Button'
+
+interface Props {
+    className?: string
+    tags: string[]
+    styleOverride?: StyleType
+}
+
+export class Tags extends React.Component<Props> {
+    public render() {
+        const { children, className, styleOverride, tags, ...restProps } = this.props
+
+        return (
+            <ul className={this.getClassName()} {...restProps}>
+                {tags.map(tag => {
+                    return (
+                        <li key={tag}>
+                            <Button styleOverride={styleOverride} type='button'>{tag}</Button>
+                        </li>
+                    )
+                })}
+            </ul>
+        )
+    }
+
+    private getClassName() {
+        const { className } = this.props
+
+        return classnames('Tags', {}, className)
+    }
+}
