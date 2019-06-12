1a6be3eb06f52c903c889b17c69017597af36e24
Chelsea <chelsea.doeleman@gmail.com>
2019-06-12 12:16:08 +0200

    feature(RawData): add RawData

---
+++ client/src/scripts/components/RawData/RawData.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import classnames from 'classnames'
+import './RawData.scss'
+import { IconNames, Icon } from '../Icon/Icon'
+
+type dataTypes = 'pdf' | 'xml' | 'csv' | 'tsv'
+
+interface Props {
+    className?: string
+    onClick?: React.MouseEventHandler<HTMLDivElement>
+    title?: string
+    fileTypes?: dataTypes
+    iconName?: IconNames
+}
+
+export class RawData extends React.Component<Props> {
+    public render() {
+        const { children, className, fileTypes, title, iconName, ...restProps } = this.props
+
+        return (
+            <div className={this.getClassName()} {...restProps}>
+                {iconName === fileTypes &&
+                    <a href={'/'} download>
+                        <Icon iconName={fileTypes}/>
+                        { title }
+                    </a>
+                }
+            </div>
+        )
+    }
+
+    private getClassName() {
+        const { className } = this.props
+
+        return classnames('RawData', {}, className)
+    }
+}
+        
