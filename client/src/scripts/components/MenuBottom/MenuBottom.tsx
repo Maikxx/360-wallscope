ff0cc5493812100162368bf0343c6df5d42695a1
Chelsea <chelsea.doeleman@gmail.com>
2019-06-18 15:04:28 +0200

    feature(MenuButton): add gooey button to menu

--- client/src/scripts/components/MenuBottom/MenuBottom.tsx
+++ client/src/scripts/components/MenuBottom/MenuBottom.tsx
@@ -5,34 +5,84 @@ import { routes } from '../../routes'
 import { NavLink } from '../NavLink/NavLink'
 import { Button } from '../Button/Button'
 import './MenuBottom.scss'
+import { IconNames, Icon } from '../Icon/Icon'
 
 interface Props {
     className?: string
     fullName?: string | null
+    iconName?: IconNames
 }
 
-export class MenuBottom extends React.Component<Props> {
+interface State {
+    isOpen?: boolean
+}
+
+export class MenuBottom extends React.Component<Props, State> {
+    public state = {
+        isOpen: false,
+    }
+
     public render() {
-        const { fullName } = this.props
+        const { fullName, iconName } = this.props
         const isLoggedIn = fullName ? true : false
 
         return (
             <Navigation className={this.getClassName()}>
                 <NavLink
-                    route={isLoggedIn ? routes.App.Boards.index : routes.App.index}
+                    route={isLoggedIn ? routes.App.Boards.index : routes.App.CurrentUser.index}
                     iconName='boards'
                     color='#CEC7EC'
                 >
-                    {isLoggedIn ? 'Boards' : 'Home'}
+                    {isLoggedIn ? 'Boards' : 'Login for more fun features'}
                 </NavLink>
-                <li>
-                    <Button
-                        className='Button_big'
-                        styleOverride='big-button'
-                        type='button'
-                        iconName='search_big'
-                        color='#181631'
-                    />
+                <li className='Middle'>
+                    {iconName === 'pen' ?
+                    <React.Fragment>
+                        <div className='menu'>
+                            <input type='checkbox' className='menu-open' name='menu-open' id='menu-open'/>
+                            <label className='menu-open-button' htmlFor='menu-open'>
+                                <Icon iconName='pen' color='#181631'/>
+                            </label>
+                            <a className='menu-item'>
+                                <Icon iconName='pen' color='#181631'/>
+                            </a>
+                            <a className='menu-item'>
+                                <Icon iconName='pen' color='#181631'/>
+                            </a>
+                            <a className='menu-item'>
+                                <Icon iconName='pen' color='#181631'/>
+                            </a>
+                        </div>
+
+                        <svg xmlns='http://www.w3.org/2000/svg' version='1.1'>
+                            <defs>
+                              <filter id='shadowed-goo'>
+
+                                  <feGaussianBlur in='SourceGraphic' result='blur' stdDeviation='10' />
+                                  <feColorMatrix in='blur' mode='matrix' values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7' result='goo' />
+                                  <feGaussianBlur in='goo' stdDeviation='3' result='shadow' />
+                                  <feColorMatrix in='shadow' mode='matrix' values='0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 -0.2' result='shadow' />
+                                  <feOffset in='shadow' dx='1' dy='1' result='shadow' />
+                                  <feComposite in2='shadow' in='goo' result='goo' />
+                                  <feComposite in2='goo' in='SourceGraphic' result='mix' />
+                              </filter>
+                              <filter id='goo'>
+                                  <feGaussianBlur in='SourceGraphic' result='blur' stdDeviation='10' />
+                                  <feColorMatrix in='blur' mode='matrix' values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7' result='goo' />
+                                  <feComposite in2='goo' in='SourceGraphic' result='mix' />
+                              </filter>
+                            </defs>
+                        </svg>
+                    </React.Fragment>
+                    :
+                        <Button
+                            className='Button_big'
+                            styleOverride='big-button'
+                            type='button'
+                            iconName={iconName}
+                            color='#181631'
+                        />
+                    }
                 </li>
                 <NavLink
                     route={isLoggedIn ? routes.App.CurrentUser.index : routes.Login.index}
