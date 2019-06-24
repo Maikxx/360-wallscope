876de92acd84e2858fb6fa59c60d2b0893ee34c5
Chelsea <chelsea.doeleman@gmail.com>
2019-06-24 16:42:35 +0200

    refactor(SearchQuery): remove Tags from SearchQuery

--- client/src/scripts/components/SearchQuery/SearchQuery.tsx
+++ client/src/scripts/components/SearchQuery/SearchQuery.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import classnames from 'classnames'
 import './SearchQuery.scss'
-import { Tags } from '../Tags/Tags'
 import { Form } from '../Form/Form'
 import { Button } from '../Button/Button'
 import { TextArea } from '../Form/TextArea/TextArea'
@@ -10,17 +9,15 @@ interface Props {
     className?: string
     onClick?: React.MouseEventHandler<HTMLDivElement>
     title?: string
-    tags: string[]
     searchWords?: string
 }
 
 export class SearchQuery extends React.Component<Props> {
     public render() {
-        const { children, className, searchWords, title, tags, ...restProps } = this.props
+        const { children, className, searchWords, title, ...restProps } = this.props
 
         return (
             <div className={this.getClassName()} {...restProps}>
-                <Tags styleOverride={'tag-border-button'} tags={tags}/>
                 <Form onSubmit={this.onSubmit} className={'SearchQuery__form'}>
                     <TextArea
                         name='search'
