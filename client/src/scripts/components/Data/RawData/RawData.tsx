a88575689f705c223111203054d02e6c5a6a2213
Maikel van Veen <maikeloo10@hotmail.com>
2019-06-26 11:13:35 +0200

    fix(RawData): remove excess property

--- client/src/scripts/components/Data/RawData/RawData.tsx
+++ client/src/scripts/components/Data/RawData/RawData.tsx
@@ -37,7 +37,7 @@ export class RawData extends React.Component<Props, State> {
     }
 
     public render() {
-        const { children, className, iconName, file, boardNames, user, ...restProps } = this.props
+        const { children, className, iconName, file, boardNames, user, onCreateNewBoard, ...restProps } = this.props
 
         return (
             <div className={`DataWrapper`} >
