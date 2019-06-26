ac01fe8a2d3eb7056589060458fc5939f5bfad4d
Chelsea <chelsea.doeleman@gmail.com>
2019-06-26 13:37:43 +0200

    feature(HomeView): add Sidebar to home

--- client/src/scripts/views/HomeView/HomeView.tsx
+++ client/src/scripts/views/HomeView/HomeView.tsx
@@ -7,6 +7,7 @@ import { Header } from '../../components/Header/Header'
 import { RouteComponentProps } from 'react-router'
 import { routes } from '../../routes'
 import { CurrentUserContext } from '../../services/UserService'
+import { Sidebar } from '../../components/SideBar/Sidebar'
 
 interface Props extends RouteComponentProps {
     onChangeSearch: (searchQuestion: string) => void
@@ -21,6 +22,23 @@ export class HomeView extends React.Component<Props> {
                 {user => (
                     <View>
                         <Header back={false} more={false}/>
+                        <Sidebar>
+                            <h1>Welcome to RAIN!</h1>
+                            <p>
+                                Rain is an application, where you can browse
+                                through medical data from ISD Scotland.
+                            </p>
+                            <ol>
+                                <li>Just search for information like you normally do.</li>
+                                <li>To access more features create an account or log in.</li>
+                                <li>Add articles or datasets to your own personal boards.</li>
+                                <li>
+                                    Create your own research by linking data to one another,
+                                    where you think there might be any correlation.
+                                </li>
+                                <li>Collaborate with other users and expand your research!</li>
+                            </ol>
+                        </Sidebar >
                         <HomeIntroduction />
                         <Search onSearch={this.onSearch}/>
                         <MenuBottom fullName={user && user.fullName} iconName='search_big'/>
